d5e9aaea9a2931c2246df946b5885f948af9f130
Fatima Hiprenoridel Hizon <fhchizon@gmail.com>
2018-04-03 12:21:36 +0800

    First Commit

---
+++ src/main/java/com/acn/acit/watsonservices/texttospeech/TextToSpeechDemo.java
@@ -0,0 +1,22 @@
+package com.acn.acit.watsonservices.texttospeech;
+
+public class TextToSpeechDemo {
+	/*	
+	 * Please refer on Text_To_Speech_HandsOn.ppt
+	 * Visit API Reference https://www.ibm.com/watson/developercloud/text-to-speech/api/v1/
+	 * Copy Maven dependency in pom.xml,
+	 * Update the pom dependency: Right click on project > Maven > Update Project
+	 * On API Reference: Click on JAVA tab.
+	 * On API Reference: Click on Synthesis > Synthesize audio
+	 * Copy and paste the code inside public static void main(String[] args){}
+	 * Import necessary libraries
+	 * Copy and change the version, username, password based on your tone analyzer service credentials.
+	 * To run using in eclipse: Right click on the project in eclipse > Run As > Java Application
+	 * To run using command prompt: Go to the project directory and type the command > 'mvn exec:java'
+	 * To rebuild type 'mvn package' or 'mvn verify' before executing 'mvn exec:java' command
+	*/
+	public static void main(String[] args) {
+		//Paste code here...
+	}
+
+}
